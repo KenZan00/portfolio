4d58d83ccf475009d4a8292b2a74fcff9b9d04b7
KenZan00 <akerup.kenny@gmail.com>
2026-06-21 17:15:47 +0200

    Component now works solely with JSON format

---
+++ src/pages/api/contact-me.ts
@@ -0,0 +1,25 @@
+import { Resend } from 'resend';
+
+const apiKeyResend = import.meta.env.API_KEY_RESEND
+const resend = new Resend(apiKeyResend);
+
+export async function POST({ request }: { request: Request }) {
+    const { email, message } = await request.json();
+    console.log('Server received:', { email, message });
+
+    const {data, error} = await resend.emails.send({
+        from: "My portfolio <onboarding@resend.dev>",
+        to: import.meta.env.MY_EMAIL,
+        subject: "Message from portfolio contact form",
+        replyTo: email,
+        text: message,
+    });
+
+    if (error) {
+        console.error('Error sending email:', error);
+        return new Response("Not ok", {status: 500});
+    }
+
+    console.log({ data });
+    return Response.json({ ok: true });
+}
