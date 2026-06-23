1c6ac347254ac244a11a937d9fdf25eee00c3dd7
KenZan00 <akerup.kenny@gmail.com>
2026-06-23 21:58:21 +0200

    Collection definition for my projects

---
+++ src/content.config.ts
@@ -0,0 +1,16 @@
+import { defineCollection } from 'astro:content';
+import { glob, file } from 'astro/loaders';
+import { z } from 'astro/zod';
+
+const projectCollection = defineCollection({
+    loader: glob({ base: './src/content/projects', pattern: '**/*.md' }),
+    schema: z.object({
+        title: z.string(),
+        image: z.string(),
+        tech: z.array(z.string()),
+        description: z.string(),
+        github: z.url(),
+    })
+})
+
+export const collections = { projectCollection };
